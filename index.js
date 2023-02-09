11753ccf611d7ccc57619a7245203e037bbc578e
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-08 19:35:09 -0700

    complete all tests

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Shelby",
+    streetAddress: "2906 S Joslin",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+      ...obj,
+      [key]: value,
+    };  
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+};
